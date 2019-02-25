0ec893c5797d0e3dc89e6ce301778239ed62c9d8
Ubuntu <ubuntu@ip-172-31-81-3.ec2.internal>
2019-02-25 22:21:09 +0000

    new code added

---
+++ src/main/java/com/ApplicationDept/app/App.java
@@ -0,0 +1,13 @@
+package com.ApplicationDept.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
